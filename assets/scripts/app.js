15ccd0e1bdc332187f39713f89f191c370914b25
Akshay Vinod Punnath <akshayvinodpunnath@gmail.com>
2020-05-04 23:18:06 +0530

    function named add created

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,10 @@
 const defaultResult = 0;
 let currentResult = defaultResult ;
 
+function add(num1, num2) {
+    return num1 + num2;
+}
+
 currentResult = (currentResult + 10) * 3 / 2 - 1;
 
 let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
