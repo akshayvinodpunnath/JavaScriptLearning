2f71fad7481ced08f71e300e4ad7c0e81e5abdca
Akshay Vinod Punnath <akshayvinodpunnath@gmail.com>
2020-05-04 21:29:16 +0530

    defaultResult const variable defined and assinged to currentResult

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,8 @@
-let currentResult = 0 ;
+const defaultResult = 0;
+let currentResult = defaultResult ;
 
 currentResult = currentResult + 10;
 
-let calculationDescription = '0 + 10'
+let calculationDescription = String(defaultResult) + ' + 10';
+
 outputResult(currentResult, calculationDescription);
