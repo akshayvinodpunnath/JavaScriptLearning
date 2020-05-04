5920161a530190aeaf8d632019a5836b517f4309
Akshay Vinod Punnath <akshayvinodpunnath@gmail.com>
2020-05-04 21:35:46 +0530

    calculationDescription modified to use backticks

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,8 +1,8 @@
 const defaultResult = 0;
 let currentResult = defaultResult ;
 
-currentResult = currentResult + 10;
+currentResult = (currentResult + 10) * 3 / 2 - 1;
 
-let calculationDescription = defaultResult + ' + 10';
+let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
 
 outputResult(currentResult, calculationDescription);
