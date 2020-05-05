6fd1034eb9ed6e5ea04cbbc14a1ba7643109c574
Akshay Vinod Punnath <akshayvinodpunnath@gmail.com>
2020-05-05 21:36:39 +0530

    addBtn.addEventListener added to app.js

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,15 +1,9 @@
 const defaultResult = 0;
 let currentResult = defaultResult ;
 
-function add(num1, num2) {
-    //alert(`The result is: ${num1 + num2}`) ;
-    return num1 + num2;
+function add() {
+    currentResult = currentResult + userInput.value;
+    outputResult(currentResult, '');
 }
 
-const additionalResult = add(1, 2);
-
-currentResult = (currentResult + 10) * 3 / 2 - 1;
-
-let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
-
-outputResult(currentResult, calculationDescription);
+addBtn.addEventListener('click', add);
