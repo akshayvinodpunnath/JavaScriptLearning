5eca993de19f784c27319391104904a67d3ac22d
Akshay Vinod Punnath <akshayvinodpunnath@gmail.com>
2020-05-05 22:41:56 +0530

    Adding comments

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,15 +1,18 @@
 const defaultResult = 0;
 let currentResult = defaultResult ;
 
+// Gets input from input field
 function getUserInput () {
     return parseInt(userInput.value);
 }
 
+// Generates and writes calculation log
 function createAndWriteOutput (operator, resultBeforeCalc, calcNumber) {
     const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
-    outputResult(currentResult, calcDescription);
+    outputResult(currentResult, calcDescription); // from vendor file
 }
 
+// Add functionality
 function add() {
     const enteredNumber = getUserInput();
     const initialResult = currentResult;
@@ -17,6 +20,7 @@ function add() {
     createAndWriteOutput('+',initialResult,enteredNumber);
 }
 
+// Multiply functionality
 function multiply() {
     const enteredNumber = getUserInput();
     const initialResult = currentResult;
@@ -24,6 +28,7 @@ function multiply() {
     createAndWriteOutput('*',initialResult,enteredNumber);
 }
 
+// Subtract functionality
 function subtract() {
     const enteredNumber = getUserInput();
     const initialResult = currentResult;
@@ -31,7 +36,7 @@ function subtract() {
     createAndWriteOutput('-',initialResult,enteredNumber);
 }
 
-
+// Divide functionality
 function divide() {
     const enteredNumber = getUserInput();
     const initialResult = currentResult;
@@ -39,6 +44,7 @@ function divide() {
     createAndWriteOutput('/',initialResult,enteredNumber);
 }
 
+// Calling Event Handlers
 addBtn.addEventListener('click', add);
 subtractBtn.addEventListener('click', subtract);
 multiplyBtn.addEventListener('click', multiply);
