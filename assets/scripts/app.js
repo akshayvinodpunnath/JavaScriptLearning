95fc7a1e0fde6d8805cbb15876715af99af22de7
Akshay Vinod Punnath <akshayvinodpunnath@gmail.com>
2020-05-05 22:12:56 +0530

    Adding eventListeners for subtract, multiply, divide

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,8 +2,26 @@ const defaultResult = 0;
 let currentResult = defaultResult ;
 
 function add() {
-    currentResult = currentResult + userInput.value;
+    currentResult = currentResult + parseInt(userInput.value);
+    outputResult(currentResult, '');
+}
+
+function multiply() {
+    currentResult = currentResult * parseInt(userInput.value);
+    outputResult(currentResult, '');
+}
+
+function subtract() {
+    currentResult = currentResult - parseInt(userInput.value);
+    outputResult(currentResult, '');
+}
+
+function divide() {
+    currentResult = currentResult / parseInt(userInput.value);
     outputResult(currentResult, '');
 }
 
 addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
