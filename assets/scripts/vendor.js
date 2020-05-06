bd639ba8331d269bc374d3860be41bae0c3dc3e6
Akshay Vinod Punnath <akshayvinodpunnath@gmail.com>
2020-05-06 22:31:22 +0530

    DOM manipulation variable defined in vendor.js

--- assets/scripts/vendor.js
+++ assets/scripts/vendor.js
@@ -4,7 +4,16 @@ const subtractBtn = document.getElementById('btn-subtract');
 const multiplyBtn = document.getElementById('btn-multiply');
 const divideBtn = document.getElementById('btn-divide');
 const resetCalc = document.getElementById('reset-calc');
-
+const zeroBtn = document.getElementById('btn-zero');
+const oneBtn = document.getElementById('btn-one');
+const twoBtn = document.getElementById('btn-two');
+const threeBtn = document.getElementById('btn-three');
+const fourBtn = document.getElementById('btn-four');
+const fiveBtn = document.getElementById('btn-five');
+const sixBtn = document.getElementById('btn-six');
+const sevenBtn = document.getElementById('btn-seven');
+const eightBtn = document.getElementById('btn-eight');
+const nineBtn = document.getElementById('btn-nine');
 
 const currentResultOutput = document.getElementById('current-result');
 const currentCalculationOutput = document.getElementById('current-calculation');
